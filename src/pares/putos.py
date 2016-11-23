bb33260da13e9b39174e40f0cf1014168acbda62
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2016-11-23 09:15:22 -0600

    empezado heap sort

--- src/pares/putos.py
+++ src/pares/putos.py
@@ -6,11 +6,62 @@ Created on 21/11/2016
 https://www.hackerrank.com/challenges/pairs
 '''
 import logging
+import heapq
+import sys
 
 
 nivel_log = logging.ERROR
 nivel_log = logging.DEBUG
 logger_cagada = None
+
+
+class PriorityQueue(object):
+    """Priority queue based on heap, capable of inserting a new node with
+    desired priority, updating the priority of an existing node and deleting
+    an abitrary node while keeping invariant"""
+
+    def __init__(self, heap=[]):
+        """if 'heap' is not empty, make sure it's heapified"""
+
+        heapq.heapify(heap)
+        self.heap = heap
+        self.entry_finder = dict({i[-1]: i for i in heap})
+        logger_cagada.debug("el finder es %s" % self.entry_finder)
+        self.REMOVED = sys.maxsize
+
+    def insert(self, node, priority=0):
+        """'entry_finder' bookkeeps all valid entries, which are bonded in
+        'heap'. Changing an entry in either leads to changes in both."""
+
+        if node in self.entry_finder:
+            self.delete(node)
+        entry = [priority, node]
+        self.entry_finder[node] = entry
+        heapq.heappush(self.heap, entry)
+        logger_cagada.debug("el finde aora es %s" % self.entry_finder)
+        logger_cagada.debug("el heap aora es %s" % self.heap)
+
+    def delete(self, node):
+        """Instead of breaking invariant by direct removal of an entry, mark
+        the entry as "REMOVED" in 'heap' and remove it from 'entry_finder'.
+        Logic in 'pop()' properly takes care of the deleted nodes."""
+
+        logger_cagada.debug("norrando nodo %s" % node)
+        entry = self.entry_finder.pop(node)
+        entry[-1] = self.REMOVED
+        return entry[0]
+
+    def pop(self):
+        """Any popped node marked by "REMOVED" does not return, the deleted
+        nodes might be popped or still in heap, either case is fine."""
+
+        while self.heap:
+            logger_cagada.debug("elem de heap %s" % self.heap)
+            priority, node = heapq.heappop(self.heap)
+            if node is not self.REMOVED:
+                del self.entry_finder[node]
+                return priority, node
+        raise KeyError('pop from an empty priority queue')
 
 # Head ends here
 def pairs(a, k):
